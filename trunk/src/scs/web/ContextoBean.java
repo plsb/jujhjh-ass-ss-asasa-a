e847adcdf43def2e09116be05480c89feca99bb1
peluissaraiva <peluissaraiva@444ddef9-b7f6-b8cf-122c-d1a8dd4a93ef>
2012-08-20 21:44:25 +0000

    primeiro comit

---
+++ trunk/src/scs/web/ContextoBean.java
@@ -0,0 +1,35 @@
+package scs.web;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import scs.usuario.Usuario;
+import scs.usuario.UsuarioRN;
+
+@ManagedBean(name="contextBean")
+@SessionScoped
+public class ContextoBean {
+	private Usuario usuariologado = null;
+	
+	public Usuario getUsuarioLogado(){
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext external = context.getExternalContext();
+		String login = external.getRemoteUser();
+		if(this.usuariologado==null||!login.equals(this.usuariologado.getLogin())){
+			if(login!=null){
+				UsuarioRN usuarioRN = new UsuarioRN();
+				this.usuariologado = usuarioRN.buscarPorLogin(login);
+			}
+		}
+		return usuariologado;
+	}
+	
+	public void setUsuarioLogado(Usuario usuario){
+		this.usuariologado=usuario;
+	}
+
+}
---
+++ trunk/src/scs/web/filter/ConexaoHibernateFilter.java
@@ -0,0 +1,79 @@
+/*
+ * Código-fonte do livro "Programação Java para a Web"
+ * Autores: Décio Heinzelmann Luckow <decioluckow@gmail.com>
+ *          Alexandre Altair de Melo <alexandremelo.br@gmail.com>
+ *
+ * ISBN: 978-85-7522-238-6
+ * http://www.javaparaweb.com.br
+ * http://www.novatec.com.br/livros/javaparaweb
+ * Editora Novatec, 2010 - todos os direitos reservados
+ *
+ * LICENÇA: Este arquivo-fonte está sujeito a Atribuição 2.5 Brasil, da licença Creative Commons,
+ * que encontra-se disponível no seguinte endereço URI: http://creativecommons.org/licenses/by/2.5/br/
+ * Se você não recebeu uma cópia desta licença, e não conseguiu obtê-la pela internet, por favor,
+ * envie uma notificação aos seus autores para que eles possam enviá-la para você imediatamente.
+ *
+ *
+ * Source-code of "Programação Java para a Web" book
+ * Authors: Décio Heinzelmann Luckow <decioluckow@gmail.com>
+ *          Alexandre Altair de Melo <alexandremelo.br@gmail.com>
+ *
+ * ISBN: 978-85-7522-238-6
+ * http://www.javaparaweb.com.br
+ * http://www.novatec.com.br/livros/javaparaweb
+ * Editora Novatec, 2010 - all rights reserved
+ *
+ * LICENSE: This source file is subject to Attribution version 2.5 Brazil of the Creative Commons
+ * license that is available through the following URI:  http://creativecommons.org/licenses/by/2.5/br/
+ * If you did not receive a copy of this license and are unable to obtain it through the web, please
+ * send a note to the authors so they can mail you a copy immediately.
+ *
+ */
+package scs.web.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.hibernate.SessionFactory;
+
+import scs.util.HibernateUtil;
+
+public class ConexaoHibernateFilter implements Filter {
+
+	private SessionFactory	sf;
+
+	public void init(FilterConfig config) throws ServletException {
+		this.sf = HibernateUtil.getSessionFactory();
+	}
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws ServletException {
+
+		try {
+
+			this.sf.getCurrentSession().beginTransaction();
+
+			chain.doFilter(servletRequest, servletResponse);
+
+			this.sf.getCurrentSession().getTransaction().commit();
+			this.sf.getCurrentSession().close();
+
+		} catch (Throwable ex) {
+			try {
+				if (this.sf.getCurrentSession().getTransaction().isActive()) {
+					this.sf.getCurrentSession().getTransaction().rollback();
+				}
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+			throw new ServletException(ex);
+		}
+	}
+
+}
