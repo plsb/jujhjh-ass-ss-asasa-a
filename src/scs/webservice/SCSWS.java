1ce56499643df0717564690c2efa934cd3c090be
peluissaraiva <peluissaraiva@444ddef9-b7f6-b8cf-122c-d1a8dd4a93ef>
2012-08-29 20:27:26 +0000

    Criado WebService

---
+++ src/scs/webservice/SCSWS.java
@@ -0,0 +1,29 @@
+package scs.webservice;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import scs.unidade.Unidade;
+import scs.unidade.UnidadeRN;
+import scs.usuario.Usuario;
+import scs.usuario.UsuarioRN;
+
+@WebService
+public class SCSWS {
+	
+	@WebMethod
+	public List<Usuario> ListarUsuariosFuncionarios(){
+		UsuarioRN usu = new UsuarioRN();
+		return usu.listar();
+	}
+	
+	@WebMethod
+	public List<Unidade> ListarUnidades(){
+		UnidadeRN uni = new UnidadeRN();
+		return uni.listar();
+	}
+
+}
